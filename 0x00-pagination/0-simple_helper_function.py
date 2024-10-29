3918f147c3b9a6ba56f77399742d7cf42e035b4c
JosepharDev <josephardev@gmail.com>
2024-10-29 08:16:32 +0000

    fix syntax error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Simple pagination function helper"""
 
-from Typing import Tuple
+from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
