739cf1674ba80ddf4210375064c9194402321c4c
JosepharDev <josephardev@gmail.com>
2024-11-13 09:05:32 +0000

    determine the best match with our supported languages from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+    flask app that render a template
+"""
+from flask import Flask
+from flask import request
+from flask_babel import Babel
+from flask import render_template
+
+
+class Config:
+    """Config Class"""
+    LANGUAGE = ['en', 'fr']
+    BABEL_DEFAULT_LOCAL = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_local() -> str:
+    """
+        determine the best match with our supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """
+        root view to render a root template
+    """
+    return render_template('1-index.html',)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
